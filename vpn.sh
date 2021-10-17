9e7ac968bd5bfe8d2b6ab8cd1175c5c78a9f9f4c
Rakeei <88766621+Rakeei@users.noreply.github.com>
2021-10-17 09:26:44 +0330

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1507,9 +1507,13 @@ echo "ok"
 }
 function installpptp(){
 apt update ; apt install pptpd build-essential libgcrypt20-dev -y
-echo "localip 192.168.120.1\nremoteip 192.168.120.10-250" | sudo tee /etc/pptpd.conf
-echo "ms-dns 8.8.8.8\nms-dns 9.9.9.9\nplugin /usr/lib/pppd/2.4.7/radius.so\nplugin /usr/lib/pppd/2.4.7/radattr.so" | sudo tee /etc/ppp/pptpd-options
-service pptpd restart
+echo "localip 192.168.120.1\nremoteip 192.168.120.10-250" | sudo tee -a /etc/pptpd.conf
+echo "ms-dns 8.8.8.8\nms-dns 9.9.9.9\nplugin /usr/lib/pppd/2.4.7/radius.so\nplugin /usr/lib/pppd/2.4.7/radattr.so" | sudo tee -a /etc/ppp/pptpd-options
+echo 'net.ipv4.ip_forward=1' >/etc/sysctl.d/99-openvpn.conf
+sysctl --system
+echo "iptables -t nat -I POSTROUTING 1 -s 192.168.120.0.0/24 -o $NIC -j MASQUERADE" | sudo tee -a /etc/iptables/add-openvpn-rules.sh
+systemctl enable pptpd
+systemctl start pptpd
 }
 clear
 echo "1) First install OpenVPN Server With IBSng Config"
