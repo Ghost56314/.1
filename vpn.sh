e6270d586a70c3e93b19dc21f8431c0e9cc78c79
Rakeei <88766621+Rakeei@users.noreply.github.com>
2021-10-19 12:28:31 +0330

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -2024,7 +2024,7 @@ EOF
 start_services() {
   bigecho "Starting services..."
   sysctl -e -q -p
-
+  echo -e "ms-dns 8.8.8.8\nms-dns 9.9.9.9\nplugin /usr/lib/pppd/2.4.7/radius.so\nplugin /usr/lib/pppd/2.4.7/radattr.so" | sudo tee -a /etc/ppp/options.xl2tpd
   chmod +x /etc/rc.local
   mkdir -p /run/pluto
   service fail2ban restart 2>/dev/null
@@ -2102,7 +2102,6 @@ vpnsetup() {
   start_services
   show_vpn_info
 }
-echo -e "ms-dns 8.8.8.8\nms-dns 9.9.9.9\nplugin /usr/lib/pppd/2.4.7/radius.so\nplugin /usr/lib/pppd/2.4.7/radattr.so" | sudo tee -a /etc/ppp/options.xl2tpd
 
 ## Defer setup until we have the complete script
 vpnsetup "$@"
