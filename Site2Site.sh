c836be17d94c05a1f5fb4e552372d4957f497b6a
Rakeei <88766621+Rakeei@users.noreply.github.com>
2022-01-07 11:16:35 +0330

    Update Site2Site.sh

--- Site2Site.sh
+++ Site2Site.sh
@@ -1,1 +1,16 @@
-#/bin/bash
+#!/bin/bash
+PUBLICIP=$(curl -s https://api.ipify.org)
+        until [[ $ENDPOINT =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; do
+        read -rp "Public Foreign address or hostname: " -e -i "$PUBLICIP" ENDPOINT
+done
+
+IRIP=$(curl -s https://api.ipify.org)
+        until [[ $IRPOINT =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$ ]]; do
+        read -rp "Public IRAN address or hostname: " -e -i "$IRIP" IRPOINT
+done
+
+ip tunnel add gre1 mode gre local $PUBLICIP remote $IRIP ttl 255
+ip addr add 10.0.0.1/30 dev gre1
+ip link set gre1 up
+iptables -t nat -A POSTROUTING -s 10.0.0.0/30 ! -o gre+ -j SNAT --to-source $PUBLICIP
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -o gre+ -j SNAT --to-source $PUBLICIP
